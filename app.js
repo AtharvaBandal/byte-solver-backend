f9d54d8a05a7007c9116c37630e1acaf3971c1b3
Atharva Bandal <bandalatharva@gmail.com>
2023-09-07 08:01:38 +0530

    some changes cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://college-club-website-client.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://chic-faloodeh-63c56a.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
